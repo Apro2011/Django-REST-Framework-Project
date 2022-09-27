8b392677b82d78da8841cde808db0bd2286c885d
Aprotim Biswas <aprotim1983@example.com>
2022-09-27 15:04:19 +0530

    Ignored some files

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b9*idnt%4hv(gt!v)jk1pwmi!!ek#nyshgyp1h0n+4h*9v!dd=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "*"]
 
 
 # Application definition
